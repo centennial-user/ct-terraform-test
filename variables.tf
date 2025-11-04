35607e11578a1f86b6abe6e0b7dd63f8b28ac3da
centennial-user <suresh.puram@centennial-tech.com>
2025-11-04 15:34:33 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the Subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
