ae49162a4dafe3a545368999555db5f106b0a798
centennial-user <suresh.puram@centennial-tech.com>
2025-11-04 15:32:11 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = var.region
+}
+# Create VPC
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+# Create Subnet
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
+# Create Route Table (Local only)
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "MyRouteTable"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+
+# Create Security Group
+
+resource "aws_security_group" "sg" {
+  name        = "MySecurityGroup"
+  description = "Allow internal VPC traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  tags = {
+    Name = "MySecurityGroup"
+  }
+}
+
